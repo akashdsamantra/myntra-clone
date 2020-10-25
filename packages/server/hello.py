e845cfdfaff6585ab5953142e4b5285664bbc028
Akashdeep Samantra <akash.samantra19@gmail.com>
2020-10-25 17:09:43 +0530

    Configuration of django and graphene

--- packages/server/hello.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-def hello_world(order2, blogId):
-   return 'Hello World %d %d' % (blogId, order2)
-
-if __name__ == '__main__':
-   app.run(debug = True)
