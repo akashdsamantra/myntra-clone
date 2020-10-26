87b1579666dd91ce6ff3383b2b8dd33ffaa603a3
Akashdeep Samantra <akash.samantra19@gmail.com>
2020-10-26 23:01:43 +0530

    Add .editorconfig

--- packages/server/myauth/models.py
+++ packages/server/myauth/models.py
@@ -3,27 +3,27 @@ from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 class User(AbstractBaseUser):
-  mobile = models.CharField(max_length=10)
-  first_name = models.CharField(max_length=256)
-  middle_name = models.CharField(max_length=256)
-  last_name = models.CharField(max_length=256)
-  email = models.EmailField()
-  gender = models.CharField(choices=[('m', 'Male'), ('f', 'Female'), ('o', 'Other')], max_length=1)
-  dob = models.DateField()
-  is_mobile_verified = models.BooleanField(default=False)
-  is_email_verified = models.BooleanField(default=False)
+    mobile = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=256)
+    middle_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    email = models.EmailField()
+    gender = models.CharField(choices=[('m', 'Male'), ('f', 'Female'), ('o', 'Other')], max_length=1)
+    dob = models.DateField()
+    is_mobile_verified = models.BooleanField(default=False)
+    is_email_verified = models.BooleanField(default=False)
 
 OTP_TYPE_CHOICES = [
-  ('mobile-verification', 'Mobile Verification'), 
-  ('email-verification', 'Email Verification'), 
-  ('mobile-login', 'Mobile Login'), 
-  ('forgot-password', 'Forgot Password')
+    ('mobile-verification', 'Mobile Verification'),
+    ('email-verification', 'Email Verification'),
+    ('mobile-login', 'Mobile Login'),
+    ('forgot-password', 'Forgot Password')
 ]
 
 class OTP(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  email = models.EmailField()
-  mobile = models.CharField(max_length=10)
-  code = models.CharField(max_length=10)
-  type = models.CharField(choices=OTP_TYPE_CHOICES, max_length=50)
-  created = models.DateField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    mobile = models.CharField(max_length=10)
+    code = models.CharField(max_length=10)
+    type = models.CharField(choices=OTP_TYPE_CHOICES, max_length=50)
+    created = models.DateField(auto_now=True)
