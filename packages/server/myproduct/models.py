94880b37ec31a54f980cb8d318a5dca7e2bf9872
Akashdeep Samantra <akash.samantra19@gmail.com>
2020-10-31 14:59:07 +0530

    Query for products & sellers

--- packages/server/myproduct/models.py
+++ packages/server/myproduct/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-SIZE_CHOICES = [
+SIZES_CHOICES = [
     ('s', 'Small'),
     ('m', 'Medium'),
     ('l', 'Large'),
@@ -11,11 +11,11 @@ SIZE_CHOICES = [
 class Seller(models.Model):
     name = models.CharField(max_length=256)
     location = models.CharField(max_length=256)
-    address = models.CharField(max_length=256)
+    address = models.CharField(max_length=16000)
     country_of_origin = models.CharField(max_length = 256)
     email = models.EmailField(unique=True)
-    phone_number = models.PhoneNumberField(unique=True)
-    alternate_phone_number = models.PhoneNumberField(unique=True, null=True, blank=True)
+    phone_number = models.CharField(max_length=10, unique=True)
+    alternate_phone_number = models.CharField(max_length=10, unique=True, null=True, blank=True)
 
 class Product(models.Model):
     code = models.CharField(max_length=50)
@@ -24,6 +24,6 @@ class Product(models.Model):
     price = models.CharField(max_length=50)
     currency = models.CharField(max_length=10)
     discount = models.CharField(max_length=10)
-    images = ArrayField(models.CharField(max_length=256))
-    sizes_available = ArrayField(models.CharField(choices=SIZES_CHOICES, max_length=1))
+    # images = ArrayField(models.CharField(max_length=256))
+    # sizes_available = ArrayField(models.CharField(choices=SIZES_CHOICES, max_length=3))
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
