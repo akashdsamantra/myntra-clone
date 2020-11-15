c9e4a6438486c2375ae3fb10a3e61f9e0adddaef
Akashdeep Samantra <akash.samantra19@gmail.com>
2020-11-15 19:20:48 +0530

    Create a layout for auth pages

--- packages/client/src/js/apps/App.vue
+++ packages/client/src/js/apps/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <img alt="Vue logo" src="../../assets/logo.png" />
+    <div>
+        <p>Hello World</p>
+        <PageLoader text="Loading" />
+    </div>
 </template>
 
 <script>
+import PageLoader from "./components/PageLoader";
 
+export default {
+    components: { PageLoader }
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
